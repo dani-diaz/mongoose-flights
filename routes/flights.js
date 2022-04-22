b5af3e93d7965ca98397b20d602b8010ee8caa4a
dani-diaz <daniela.diaz0808@gmail.com>
2022-04-22 15:16:48 -0700

    almost done

--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var flightsCtrl = require('../controllers/flights');
-
+var ticketsCtrl = require('../controllers/tickets');
 
 router.get('/', flightsCtrl.index);
 router.get('/new', flightsCtrl.new);
+router.get('/:id', flightsCtrl.show);
+router.get('/:id/tickets/new', ticketsCtrl.new)
+
+
 router.post('/', flightsCtrl.create);
+router.post('/:id/tickets/create', ticketsCtrl.create);
+
+
 module.exports = router;
 
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 require('dotenv').config();
+
+
 var db = require('./config/database');
 var indexRouter = require('./routes/index');
 var flightsRouter = require('./routes/flights');
+var destinationsRouter = require('./routes/destinations');
 
 var app = express();
 
@@ -22,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
+app.use('/destinations', destinationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
